42dd091a66e1f0f1f11f7534726441d78c2b29a9
PhanVanLoc2001 <phanvanlocxt@gmail.com>
2021-09-19 21:27:02 +0700

    push project

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WowFoods</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id ="header">    
+        <div class ="logo"> 
+            <img src="images/logo.png" alt="" >
+        </div> 
+        <div class = "imformation">  
+                <a href="#Home" style ="color: red; margin: 0px 7px">Home</a>
+                <a href="#Contegories" style ="color: red; margin: 0px 7px;">Contegories</a>
+                <a href="#Foods" style ="color: red; margin: 0px 7px;">Foods</a>
+                <a href="#Contact" style ="color: red; margin: 0px 7px;">Contact</a>
+        </div>
+    </div>
+<div id="searchfoods">
+    
+        <input type="Search" name="search" placeholder="Search for Food...">
+        <button style="background-color:red;color:white;">Search</button>
+</div>
+<div id="explore">
+        <h1 style = "text-align: center">Explore Foods</h1>
+        <div class = "product">
+        <h1 style = "text-align: center">
+            <img src="images/pizza.jpg" style ="margin: 0px 10px;" alt="" srcset="">
+            <img src="images/burger.jpg" style ="margin: 0px 10px;" alt="" srcset="">
+            <img src="images/momo.jpg" style ="margin: 0px 10px;" alt="" srcset="">
+            </h1>
+        </div>
+</div>
+<div id = "menu">
+    <h2 style ="text-align:center">Food Menu</h2>
+    <div class = "menuorder">
+        <ul>
+            <li>
+                <img src="images/menu-pizza.jpg" style="width:100px;" alt="">
+                <h4>Food title</h4>
+                <span>$2.3</span>   
+                <h6>made with italia sauce, chiken and organice vegetables</h6>
+                <button style="background-color:red;color:white;">Oder Now</button>
+            </li>
+        </ul>
+    </div>
+    <div class = "menuorder">
+        <ul>
+            <li>
+                <img src="images/menu-pizza.jpg" style="width:100px;" alt="">
+                <h4>Food title</h4>
+                <span>$2.3</span>
+                <h6>made with italia sauce, chiken and organice vegetables</h6>
+                <button style="background-color:red;color:white;">Oder Now</button>
+            </li>
+        </ul>
+    </div>
+    <div class = "menuorder">
+        <ul>
+            <li>
+                <img src="images/menu-pizza.jpg" style="width:100px;" alt="">
+                <h4>Food title</h4> 
+                <span>$2.3</span>
+                <h6>made with italia sauce, chiken and organice vegetables</h6>
+                <button style="background-color:red;color:white;">Oder Now</button>
+            </li>
+        </ul>
+    </div>
+    <div class = "menuorder">
+        <ul>
+            <li>
+                <img src="images/menu-pizza.jpg" style="width:100px;" alt="">
+                <h4>Food title</h4>
+                <span>$2.3</span>
+                <h6>made with italia sauce, chiken and organice vegetables</h6>
+                <button style="background-color:red;color:white;">Oder Now</button>
+            </li>
+        </ul>
+    </div>
+    <h6 style="text-align:center;color:red;">See all foods</h6>
+</div>
+<div id ="signin">
+<h6 style="text-align:center">
+        <img src="images/logo-fb.png" alt="" srcset="">
+        <img src="images/logo-insta.png" alt="" srcset="">
+        <img src="images/logo-twit.png" alt="" srcset="">
+        <p style="text-align:center;">All rights Designed By <a href="#" style="color:red">Vijay Thapa</a></p>
+    </h6>
+</div>   
+
+</body>
+</html>
---
+++ order.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style-food.css">
+</head>
+<body>
+<div id ="header">    
+        <div class ="logo">
+            <img src="images/logo.png" alt="" >
+        </div> 
+        <div class = "imformation">
+            <a href="#Home" style ="color: red; margin: 0px 7px">Home</a>
+            <a href="#Contegories" style ="color: red; margin: 0px 7px;">Contegories</a>
+            <a href="#Foods" style ="color: red; margin: 0px 7px;">Foods</a>
+            <a href="#Contact" style ="color: red; margin: 0px 7px;">Contact</a>
+        </div>
+</div>
+<div class="p22" style="height: 900px; ">
+    <div class="order">
+        <h1>Fill this form to confirm your order</h1>
+        <div class="title">
+        </div>  
+        <div class="first">
+            <img src="images/menu-burger.jpg" style="width: 120px;margin: 10px 10px 10px 10px;" alt=""> 
+            <div class="nd" > 
+                <h4>Food title</h4>
+                <span>$2.3</span>
+                <p>Quantity</p> 
+                <div class="form-group"> 
+                    <input class="number" type="number" min="1" max="99" placeholder="1">
+                </div>
+            </div>
+        </div>     
+        <div>
+            
+
+        </div>  
+            <div class="secone">     
+                <div class="bodyforsecone">   
+                    <div><p >Full Name</p><input type="text" class = "label" placeholder="Ex: Phan Van Loc"> </div>
+                    <div><p>phone number</p> <input type="text" class = "label" placeholder="Ex: 02948104..."> </div> 
+                    <div> <p>Email</p> <input type="email" class = "label" placeholder="Ex: jone@gmail.com"> </div> 
+                    <div> <p>Adress</p> <textarea name="" id="" cols="62" rows="10" placeholder="Ex: stress, city, country..."></textarea> </div>
+                    <button style="background-color: salmon; color: wheat;">Submit now</button> 
+                </div>
+            </div>
+    </div>
+</div>  
+</div>  
+</body>
+</html>
